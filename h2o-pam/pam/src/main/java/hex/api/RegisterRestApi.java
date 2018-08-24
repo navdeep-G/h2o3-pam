bb168ef163f5cfd7658ecad836141d72b36a8ae2
Navdeep Gill <mr.navdeepgill@gmail.com>
2018-08-24 10:56:17 -0700

    Update h2o version and refactor PAM schema/extension to h2o-3 (#6)

--- h2o-pam/pam/src/main/java/hex/api/RegisterRestApi.java
+++ h2o-pam/pam/src/main/java/hex/api/RegisterRestApi.java
@@ -1,26 +1,19 @@
-//TODO Need work
-// /package hex.api;
-//
-//import hex.pam.PAM;
-//import water.api.*;
-//
-//public class RegisterRestApi extends AlgoAbstractRegister {
-//
-//  @Override
-//  public void registerEndPoints(RestApiContext context) {
-//    // Register k-LIME model builder REST API
-//    PAM PAMmb = new PAM(true);
-//    registerModelBuilder(context, PAMmb, SchemaServer.getStableVersion());
-//
-//    // PAM
-//    context.registerEndpoint("pam", "POST /99/PAM", PAMHandler.class, "getPAM",
-//            "Conduct PAM Clustering");
-//
-//  }
-//
-//  @Override
-//  public String getName() {
-//    return "PAM";
-//  }
-//
-//}
+package hex.api;
+
+import hex.pam.PAM;
+import water.api.*;
+
+public class RegisterRestApi extends AlgoAbstractRegister {
+
+  @Override
+  public void registerEndPoints(RestApiContext context) {
+      PAM pam = new PAM(true);
+      registerModelBuilder(context, pam, SchemaServer.getStableVersion());
+  }
+
+  @Override
+  public String getName() {
+    return "PAM";
+  }
+
+}
--- h2o-pam/pam/src/main/java/hex/schemas/PAMV3.java
+++ h2o-pam/pam/src/main/java/hex/schemas/PAMV3.java
@@ -2,15 +2,13 @@ package hex.schemas;
 
 import hex.pam.PAM;
 import hex.pam.PAMModel;
-import water.Iced;
 import water.api.API;
 import water.api.schemas3.ClusteringModelParametersSchemaV3;
 import water.api.schemas3.KeyV3;
-import water.api.schemas3.SchemaV3;
 
-public class PAMV99 extends SchemaV3<Iced, PAMV99> {
+public class PAMV3 extends ModelBuilderSchema<PAM, PAMV3, PAMV3.PAMParametersV3> {
 
-  public static final class PAMParametersV99 extends ClusteringModelParametersSchemaV3<PAMModel.PAMParameters, PAMParametersV99> {
+  public static final class PAMParametersV3 extends ClusteringModelParametersSchemaV3<PAMModel.PAMParameters, PAMParametersV3> {
     static public String[] fields = new String[] {
             "training_frame",
             "ignored_columns",

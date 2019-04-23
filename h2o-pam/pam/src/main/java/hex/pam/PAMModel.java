14206df75a9a5f2d6fbe8104b6729c8eaa26277a
Navdeep Gill <mr.navdeepgill@gmail.com>
2019-04-22 17:47:40 -0700

    Add model metrics and ability to score for a PAM model (#23)
    * Add model metrics and ability to score for a PAM model
    
    * Add comments and remove commented array
    
    * Add explanation of why we need _centers_raw

--- h2o-pam/pam/src/main/java/hex/pam/PAMModel.java
+++ h2o-pam/pam/src/main/java/hex/pam/PAMModel.java
@@ -3,8 +3,11 @@ package hex.pam;
 import hex.ModelCategory;
 import hex.ModelMetrics;
 import hex.ClusteringModel;
+import hex.ModelMetricsClustering;
 import water.H2O;
 import water.Key;
+
+import static hex.genmodel.GenModel.Kmeans_preprocessData;
 
 public class PAMModel extends ClusteringModel<PAMModel,PAMModel.PAMParameters,PAMModel.PAMOutput> {
 
@@ -23,19 +26,27 @@ public class PAMModel extends ClusteringModel<PAMModel,PAMModel.PAMParameters,PA
     public long[/*k*/] _medoid_rows; // row numbers of medoids
     public double[/*k*/][/*p*/] _medoids; // the actual medoids
     public double _sum_of_dissimilarities; // sum of dissimilarities of each observation to its medoid
+    public int[/*iterations*/] _k = new int[]{0};
     public PAMOutput( PAM b ) { super(b); }
     @Override public ModelCategory getModelCategory() { return ModelCategory.Clustering; }
   }
 
   public PAMModel(Key selfKey, PAMParameters parms, PAMOutput output) { super(selfKey,parms,output); }
 
+  @Override public ModelMetrics.MetricBuilder makeMetricBuilder(String[] domain) {
+    assert domain == null;
+    return new ModelMetricsClustering.MetricBuilderClustering(_output.nfeatures(), _parms._k);
+  }
+
   @Override
-  public ModelMetrics.MetricBuilder makeMetricBuilder(String[] domain) {
-    throw H2O.unimpl("No Model Metrics for PAMModel.");
+  protected double[] score0(double[] data, double[] preds, double offset) {
+    return score0(data, preds);
   }
 
   @Override protected double[] score0(double data[/*ncols*/], double preds[/*nclasses+1*/]) {
-    throw H2O.unimpl();
+    double[][] centers = _output._medoids;
+    preds[0] = hex.genmodel.GenModel.KMeans_closest(centers, data, _output._domains);
+    return preds;
   }
 
 }
